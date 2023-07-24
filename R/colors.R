6718c6a6364d30bc3e783523bb00b84a80b0ea84
Kelly Barnes <kbarnes@carpentries.org>
2023-07-24 12:08:44 -0400

    Added an example to carpentries_palette.
    Renamed all things called carp to carpentries.

--- R/colors.R
+++ R/colors.R
@@ -1,7 +1,7 @@
 # Carpentries color Palette
 # Based on Carpentries Brand Identity https://docs.carpentries.org/topic_folders/communications/resources/brand_identity.html
 
-carp_colors <- c(
+carpentries_colors <- c(
   black = "#383838",
   midnight = "#001483",
   fire = "#FF4955",
@@ -18,20 +18,48 @@ carp_colors <- c(
   sunshine = "#FFE7A8"
 )
 
-# manual_pal() needs to be imported from scales
-#' Title
+#' Carpentries color palette (discrete)
 #'
-#' @return
+#' This palette includes 14 colors.
+#'
+#' @return a function that generates a palette of up to 14 colors.
 #' @import scales
 #' @export
 #'
 #' @examples
-carp_pal <- function() {
-  f <- scales::manual_pal(carp_colors)
-  attr(f, "max_n") <- length(carp_colors)
+#'
+#' my_palette <- carpentries_pal()
+#' my_palette(3)
+#'
+carpentries_pal <- function() {
+  f <- scales::manual_pal(carpentries_colors)
+  attr(f, "max_n") <- length(carpentries_colors)
   f
 
 }
+
+#' Carpentries color scales
+#'
+#' @param ...
+#'
+#' @return
+#' @export
+#'
+#' @examples
+scale_fill_carp <- function(...) {
+  discrete_scale("fill", "carpentries", carpentries_pal(), ...)
+}
+
+#' @export
+#' @rdname carpentries_palette
+scale_color_carpentries <- function(...) {
+  discrete_scale("colour", "carpentries", carpentries_pal(), ...)
+}
+
+#' @export
+#' @rdname carpentries_palette
+
+scale_color_carpentries <- scale_color_carpentries
 
 # two_colors <- c(carp_fire, carp_lake)
 # three_colors <- c(carp_fire, carp_lake, carp_golden)
