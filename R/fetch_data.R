e8c361f5ff5b475cdfe8c5dd9657e6e7b16de783
Kelly Barnes <kbarnes@carpentries.org>
2023-07-26 20:58:20 -0400

    Added a dependency on utils for read.csv function.

--- R/fetch_data.R
+++ R/fetch_data.R
@@ -3,12 +3,13 @@
 #' @param query_id
 #'
 #' @return
+#' @import utils
 #' @export
 #'
 #' @examples
 fetch_redash_data <- function(query_id) {
   api_key <- Sys.getenv(paste0("REDASH_QUERY_", query_id))
   url <- paste0("https://redash.carpentries.org/api/queries/", query_id, "/results.csv?api_key=", api_key)
-  data <- read.csv(url, stringsAsFactors = FALSE)
+  data <- utils::read.csv(url, stringsAsFactors = FALSE)
   return(data)
 }
