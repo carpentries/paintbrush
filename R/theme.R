3c04f565e0bb9d8f074c6f363131413e1cc1c4c7
Kelly Barnes <kbarnes@carpentries.org>
2023-11-08 08:50:42 -0500

    updates examples

--- R/theme.R
+++ R/theme.R
@@ -14,15 +14,7 @@
 #'
 #' p1 <- ggplot(mtcars2) +
 #'   geom_point(aes(x = wt, y = mpg, colour = gear)) +
-#'   labs(
-#'     title = "Fuel economy declines as weight increases",
-#'     subtitle = "(1973-74)",
-#'     caption = "Data from the 1974 Motor Trend US magazine.",
-#'     tag = "Figure 1",
-#'     x = "Weight (1000 lbs)",
-#'     y = "Fuel economy (mpg)",
-#'     colour = "Gears"
-#'   )
+#'   theme_carpentries()
 #'
 theme_carpentries <- function() {
   theme_bw(base_size = 12) %+replace%
@@ -30,9 +22,8 @@ theme_carpentries <- function() {
       plot.title =
         element_text(size = 14, face = "bold", hjust = 0.5,
                      margin = margin(0, 0, 15, 0)),
-      axis.text = element_text(size = 9),
-      axis.text.x = element_text(angle = 0, vjust = 0.5),
-      axis.text.y = element_text(size = 9),
+      axis.text.x = element_blank(),
+      axis.ticks = element_blank(),
       axis.title = element_text(face = "bold", vjust = 0.5),
       strip.text = element_text(face = "bold"),
       strip.background = element_blank(),
